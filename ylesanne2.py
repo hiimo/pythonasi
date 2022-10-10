dd7121e9aa1f4627d6b634e3d30f228c901aac94
hiimo <115462482+hiimo@users.noreply.github.com>
2022-10-10 13:36:21 +0300

    Add files via upload

---
+++ ylesanne2.py
@@ -0,0 +1,19 @@
+#hiimo närep
+#10.10.2022
+#tsykkel
+
+import turtle
+
+#akna seaded
+aken = turtle.Screen()
+aken.setup(300,300)
+
+kl= turtle.Turtle()
+for i in range(5):
+    kl.fd(100)
+    kl.rt(144)
+    
+for i in range(3):
+    kl.color("red")
+    kl.backward(100)
+    kl.rt(120)
