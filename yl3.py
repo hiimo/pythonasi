73d5271f0fa3edb819f32f5aecad153efb55c9a6
hiimo <115462482+hiimo@users.noreply.github.com>
2022-10-10 13:40:30 +0300

    Add files via upload

---
+++ yl3.py
@@ -0,0 +1,40 @@
+import turtle
+ 
+#akna seaded
+aken = turtle.Screen()
+aken.setup(500,500)
+ 
+#kujundi loomine
+t = turtle.Turtle()
+fd = 40
+
+
+t.forward(40)
+t.left(90)
+t.fd(40)
+
+t.right(90)
+t.fd(40)
+
+t.left(90)
+t.fd(40)
+
+t.left(90)
+t.fd(40)
+
+t.right(90)
+t.fd(fd)
+
+t.left(90)
+t.fd(fd)
+
+t.left(90)
+t.fd(fd)
+
+t.right(90)
+t.fd(fd)
+
+t.
+    
+turtle.exitonclick()
+
