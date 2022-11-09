04fa1c3e8fa4030924893fa7b7e19e340dc3b478
hiimo <115462482+hiimo@users.noreply.github.com>
2022-11-09 10:01:26 +0200

    Update kontrolltoo.py

--- kontrolltoo.py
+++ kontrolltoo.py
@@ -1,3 +1,77 @@
+#hiimo
+#2.11.22
+#kontroltoo
+
+fail = open("rebased.txt", encoding="UTF-8")
+vastuvoetud = []
+aastad = [2011,2012,2013.2014,2015,2016,2017,2018,2019]
+for rida in fail:
+    vastuvoetud.append(int(rida))
+aasta = int(input("Mis aastat tahad? "))
+index = aastad.index(aasta)
+print(vastuvoetud[index])
+
+fail.close()
+
+
+
+
+
+
+m = int(input("mitu poialpossi tahab ounu? "))
+u=0
+for i in range (m):
+    n=random.randint(1,2)
+    print(n)
+    u+=n
+summa=14-u
+print(summa)
+
+
+
+
+
+
+
+#nisupask
+nisuterade = 0.5
+taisarv = int(input("sisesta taisarv: "))
+j = 0
+while j < taisarv:
+    j += 1
+    nisuterade *= 2
+
+print(nisuterade)
+
+
+
+
+
+#taring
+
+taring = int(input("mitu taringut: "))
+for i in range(taring):
+    print(random.randint(1,6))
+
+
+
+#murelikud lapsevanemad
+ringid = int(input("ringide arv: "))
+ring = 0
+porgand = 0
+while ring<ringid:
+    ring +=1
+    if  ring%2==0:
+        porgand+=ring
+print(f"porgand: {porgand}")
+
+
+#aratus
+aratus = int(input("ÄRATUS: "))
+for i in range(aratus):
+    print("tõuse ja sära")
+
+
 #tervitus
 
 print("Tere, maailm!")
