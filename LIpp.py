67cd9a2ae0b81dedee4f39ae84271caced260702
hiimo <115462482+hiimo@users.noreply.github.com>
2023-01-11 11:04:44 +0200

    Update LIpp.py

--- LIpp.py
+++ LIpp.py
@@ -9,9 +9,9 @@ louend = Canvas(aken, width=500, height=500)
 louend.pack()
 
 #kujundite loomine
-louend.create_rectangle(150, 150, 0, 0, fill='#ff0000')
-louend.create_rectangle(200,150,500,0, fill="#ff0000")
-louend.create_rectangle(150, 350, 0, 200, fill='#ff0000')
-louend.create_rectangle(200,200,500,350, fill="#ff0000")
+louend.create_rectangle(150, 150, 0, 0, fill='#ff0000', width=0)
+louend.create_rectangle(200,150,500,0, fill="#ff0000", width=0)
+louend.create_rectangle(150, 350, 0, 200, fill='#ff0000', width=0)
+louend.create_rectangle(200,200,500,350, fill="#ff0000", width=0)
 
 aken.mainloop()
